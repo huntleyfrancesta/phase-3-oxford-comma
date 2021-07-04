9ea0772c2848a4537294cda28311b38fc05aaa56
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-07-04 19:23:38 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
+def oxford_comma(input)
+    if input.length == 1
+    input.join
+    elsif input.length == 2
+        "#{input[0]} and #{input[1]}"
+    else
+    input[-1].insert(0, "and ")
+    input.join(", ")
+    end
 
 end
